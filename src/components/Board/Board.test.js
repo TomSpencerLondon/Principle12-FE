24987fd8d133f12756aae97105728dee199cf4b9
Tom <thomas.spencer@codurance.com>
2020-05-23 22:23:04 +0100

    Test for codecov

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import { jest, it, beforeEach } from '@jest/globals';
-import { act, render, waitFor } from '@testing-library/react';
-import Board from './Board';
-import { setFail } from '../../api/__mocks__/mockHelper';
-
-jest.mock('../../api/boardsApi');
-
-beforeEach(() => {
-  setFail(false);
-});
-
-function renderBoard(args) {
-  const defaultProps = {
-    setAuth: () => {}
-  };
-
-  const props = { ...defaultProps, ...args };
-
-  return render(<Board {...props} />);
-}
-
-it('shows three columns', async () => {
-  await act(async () => {
-    const { getByText } = renderBoard();
-    await waitFor(() => {
-      getByText('Start');
-      getByText('Stop');
-      getByText('Continue');
-    });
-  });
-});
-
-it('should return error message when response fails', async () => {
-  setFail(true);
-  await act(async () => {
-    const { getByText } = renderBoard();
-    await waitFor(() => {
-      getByText('Sorry something went wrong...');
-    });
-  });
-});
+// import React from 'react';
+// import { jest, it, beforeEach } from '@jest/globals';
+// import { act, render, waitFor } from '@testing-library/react';
+// import Board from './Board';
+// import { setFail } from '../../api/__mocks__/mockHelper';
+//
+// jest.mock('../../api/boardsApi');
+//
+// beforeEach(() => {
+//   setFail(false);
+// });
+//
+// function renderBoard(args) {
+//   const defaultProps = {
+//     setAuth: () => {}
+//   };
+//
+//   const props = { ...defaultProps, ...args };
+//
+//   return render(<Board {...props} />);
+// }
+//
+// it('shows three columns', async () => {
+//   await act(async () => {
+//     const { getByText } = renderBoard();
+//     await waitFor(() => {
+//       getByText('Start');
+//       getByText('Stop');
+//       getByText('Continue');
+//     });
+//   });
+// });
+//
+// it('should return error message when response fails', async () => {
+//   setFail(true);
+//   await act(async () => {
+//     const { getByText } = renderBoard();
+//     await waitFor(() => {
+//       getByText('Sorry something went wrong...');
+//     });
+//   });
+// });
--- src/components/CardForm/CardForm.test.js
+++ src/components/CardForm/CardForm.test.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { cleanup, render } from '@testing-library/react';
-import { afterEach, it } from '@jest/globals';
-import CardForm from './CardForm';
-
-
-afterEach(cleanup);
-
-function renderForm(args) {
-  const defaultProps = {
-    cards: [],
-    colId: 1,
-    handleCancelButton: () => {
-    },
-    handleAddCard: () => {
-    }
-  };
-
-  const props = { ...defaultProps, ...args };
-  return render(<CardForm {...props} />);
-}
-
-it('renders text field', () => {
-  const { getAllByText } = renderForm();
-  getAllByText('Enter a title for this card...');
-});
+// import React from 'react';
+// import { cleanup, render } from '@testing-library/react';
+// import { afterEach, it } from '@jest/globals';
+// import CardForm from './CardForm';
+//
+//
+// afterEach(cleanup);
+//
+// function renderForm(args) {
+//   const defaultProps = {
+//     cards: [],
+//     colId: 1,
+//     handleCancelButton: () => {
+//     },
+//     handleAddCard: () => {
+//     }
+//   };
+//
+//   const props = { ...defaultProps, ...args };
+//   return render(<CardForm {...props} />);
+// }
+//
+// it('renders text field', () => {
+//   const { getAllByText } = renderForm();
+//   getAllByText('Enter a title for this card...');
+// });
